72e41cc5f4598f09762349d5f1bcf9676e571335
KyleDOwens <kyledowens26@gmail.com>
2026-09-08 22:01:11 -0500

    2026 music update

--- scripts/compress_jpeg.py
+++ scripts/compress_jpeg.py
@@ -4,36 +4,39 @@ from PIL import Image
 import os
 
 
-def compressDir(target_dir, quality):
+def compressDir(target, quality):
     if (os.getcwd().split("/")[-1] != "kyles_world"):
         exit("ERROR: This script must be run from the project directory, not the script directory or anywhere else")
 
+    # Compress file
+    if os.path.isfile(target) and target.lower().endswith(('.jpg', '.jpeg')):
+        with Image.open(target) as img:
+            img.save(target, "JPEG", quality=quality, optimize=True)
+            print(f"Compressed {target} to quality {quality}")
 
-    for filename in os.listdir(target_dir):
-        if not filename.lower().endswith(('.jpg', '.jpeg')):
-            continue
+    # Compress dir
+    elif os.path.isdir(target):
+        for filename in os.listdir(target):
+            if not filename.lower().endswith(('.jpg', '.jpeg')):
+                continue
 
-        input_path = os.path.join(target_dir, filename)
-        output_path = os.path.join(target_dir, filename)
+            input_path = os.path.join(target, filename)
+            output_path = os.path.join(target, filename)
 
-        with Image.open(input_path) as img:
-            img.save(output_path, "JPEG", quality=quality, optimize=True)
-            print(f"Compressed {target_dir}/{filename} to quality {quality}")
+            with Image.open(input_path) as img:
+                img.save(output_path, "JPEG", quality=quality, optimize=True)
+                print(f"Compressed {target}/{filename} to quality {quality}")
+    
+    else:
+        print(f"ERROR: {target} not identified as file or dir")
 
 
 
 # Keep track of quality used for each dir
 dirs = [
-    # ("images/music/2018", 13),
-    # ("images/music/2019", 13),
-    # ("images/music/2020", 13),
-    # ("images/music/2021", 13),
-    # ("images/music/2022", 13), 
-    # ("images/music/2023", 13),
     # ("images/music/2024", 13),
     # ("images/music/2025", 13),
-    ("images/music/2026", 13),
-    ("images/music/favorites", 13),
+    # ("images/music/2026", 13),
     # ("images/photobook/japan", 8),
     # ("images/photobook/china", 8),
     # ("images/photobook/camino", 8),
